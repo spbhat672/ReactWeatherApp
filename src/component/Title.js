c17fc27410fe5a6341ae623c3d723852e7305cbc
Mr. Bhat <spbhat672@gmail.com>
2019-07-14 00:24:25 +0530

    Initial commit from Create React App

---
+++ src/component/Title.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+const Title = () => (
+    <div>
+        <h1 className="title-container__title">Weather Finder</h1>
+        <h3>Find Out Temperature,Conditions and more..</h3>
+    </div>
+);
+
+export default Title;
